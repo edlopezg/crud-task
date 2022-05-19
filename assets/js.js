72e4fbf961af2214a8ad57294e72e3ed43a6b1d3
Ed Jasser Lopez <jasserlopezguardado@gmail.com>
2022-05-19 02:21:24 -0500

    avance create

---
+++ assets/js.js
@@ -0,0 +1,65 @@
+
+
+
+// Esta funcion va a duplicar las tareas 
+function printTask (tasks) {
+   //identidicar el contenedor 
+    const container = document.getElementById('task-container')
+   //generar el contenido
+   let html = '';
+   for (let i = 0; i < tasks.length; i++) {
+      html += ` <div class="col-md-6 col-lg-4 mt-3">
+      
+      <div class="card" >
+       <img src='${tasks[i].image}'>
+         <div class="card-body">
+         <h5 class="card-title">${tasks[i].id}</h5>
+         <p class="card-text">${tasks[i].name}</p>
+        
+         
+          </div>
+          </div>
+          </div>
+                    `
+       
+   }
+   // imprimir el HTML
+  container.innerHTML = html;
+}
+
+
+
+// Con esta funcion le vamos a pedir las tareas al backend
+function getTask() {
+axios.get('https://e-commerce-api-academlo.herokuapp.com/api/products')
+.then(function (response) {
+        const tasks = response.data;
+        printTask(tasks);
+    })
+.catch(function (error){
+        console.log(error);
+       })
+
+}
+
+// Con esta funcion Creamos las tareas 
+function createTask (){
+    const newProduct = {
+        id: 'nuevo producto',
+        name: 'nombre del producto',
+        image: 'url'
+    }
+    axios.post('https://e-commerce-api-academlo.herokuapp.com/api/products')
+    .then(function (response) {
+         alert('Tu producto se subio correctamente')
+        console.log(response);
+       
+        
+    })
+.catch(function (error){
+        alert('Tu producto no pudo ser creado')
+        console.log(error);
+       })
+    
+}
+getTask();
